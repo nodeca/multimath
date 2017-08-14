3fbafae883416966d0ae3e2ce7a62da5746b73f7
Vitaly Puzrin <vitaly@rcdesign.ru>
2017-08-14 13:28:30 +0300

    Tests: drop standalone glur wrapper

--- test/unsharp_mask.js
+++ test/unsharp_mask.js
@@ -32,21 +32,51 @@ describe('unsharp_mask', function () {
 
     it('wasm', function () {
       const glur_js   = require('glur/mono16');
-      const glur_wasm = require('./glur_mono16_wasm');
       const mlib_wasm = mathlib_raw({ js: false }).use(require('../lib/unsharp_mask'));
+
+      // unsharp_mask wasm module does not provide API for direct glur16 call
+      // Here is simple wrapper for testing
+      function glur16_wasm_invoke(thisobj, src, width, height, radius) {
+        var elem_cnt = width * height;
+        var src_byte_cnt = elem_cnt * 2;
+        var out_byte_cnt = elem_cnt * 2;
+        var tmp_byte_cnt = elem_cnt * 2;
+        var line_byte_cnt = Math.max(width, height) * 4; // float32 array
+        var coeffs_byte_cnt = 8 * 4;
+        var src_offset = 0;
+        var out_offset = src_byte_cnt;
+        var tmp_offset = src_byte_cnt + out_byte_cnt;
+        var line_offset = src_byte_cnt + out_byte_cnt + tmp_byte_cnt;
+        var coeffs_offset = src_byte_cnt + out_byte_cnt + tmp_byte_cnt + line_byte_cnt;
+
+        var instance = thisobj.__instance(
+          'unsharp_mask',
+          src_byte_cnt + out_byte_cnt + tmp_byte_cnt + line_byte_cnt + coeffs_byte_cnt,
+          { exp: Math.exp }
+        );
+
+        var mem32 = new Uint16Array(thisobj.__memory.buffer);
+        mem32.set(src);
+
+        var fn = instance.exports.blurMono16 || instance.exports._blurMono16;
+
+        fn(src_offset, out_offset, tmp_offset, line_offset, coeffs_offset, width, height, radius);
+
+        return new Uint16Array(thisobj.__memory.buffer.slice(out_offset, out_offset + out_byte_cnt));
+      }
+
 
       return mlib_wasm.init().then(function () {
         let sample = new Uint16Array(100 * 100);
         fill(sample, [ 0, 255 ]);
 
         let sample_js   = sample.slice(0, sample.length);
-        let sample_wasm = sample.slice(0, sample.length);
 
         glur_js(sample_js, 100, 100, 2);
 
-        let blur_wasm = glur_wasm(mlib_wasm, sample_wasm, 100, 100, 2);
+        let sample_wasm = glur16_wasm_invoke(mlib_wasm, sample, 100, 100, 2);
 
-        assert.deepEqual(sample_js, blur_wasm);
+        assert.deepEqual(sample_js, sample_wasm);
       });
     });
   });

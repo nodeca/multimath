5953fa4490a23950783eec7869d477520bc4c5b6
Vitaly Puzrin <vitaly@rcdesign.ru>
2017-08-14 13:07:43 +0300

    unsharp tests cleanup

--- test/unsharp_mask.js
+++ test/unsharp_mask.js
@@ -3,88 +3,87 @@
 const assert      = require('assert');
 const mathlib_raw = require('../');
 
-function createFixture(width, height) {
-  const result = new Uint8Array(width * height * 4);
 
-  for (let i = 0; i < result.length; i++) {
-    result[i] = 20 + i;
-  }
+function fill(target, arr) {
+  if (!Array.isArray(arr)) arr = [ arr ];
 
-  return result;
+  for (let i = 0; i < target.length; i++) target[i] = arr[i % arr.length];
 }
 
-function createFixtureConst(width, height, number) {
-  const result = new Uint16Array(width * height);
-
-  for (let i = 0; i < width * height; i++) {
-    result[i] = number;
-  }
-
-  return result;
-}
-
-function createFixtureGlur(width, height) {
-  const result = new Uint16Array(width * height);
-
-  for (let i = 0; i < width * height; i++) {
-    if (i % 2) {
-      result[i] = 0;
-    } else {
-      result[i] = 255;
-    }
-  }
-
-  return result;
-}
-
-describe('glur_mono16', function () {
-  it('js', function () {
-    var glur_js = require('glur/mono16');
-    let fixture = createFixtureConst(100, 100, 33333);
-    glur_js(fixture, 100, 100, 2);
-
-    for (let i = 0; i < fixture.length; i++) {
-      assert.equal(fixture[i], 33333);
-    }
-  });
-
-  it('wasm', function () {
-    var glur_js = require('glur/mono16');
-    var glur_wasm = require('./glur_mono16_wasm');
-    const mlib_wasm = mathlib_raw({ js: false }).use(require('../lib/unsharp_mask'));
-
-    return mlib_wasm.init().then(function () {
-      var fixturejs = createFixtureGlur(100, 100);
-      glur_js(fixturejs, 100, 100, 2);
-
-      var fixture = createFixtureGlur(100, 100);
-      var blur_wasm = glur_wasm(mlib_wasm, fixture, 100, 100, 2);
-
-      assert.deepEqual(fixturejs, blur_wasm);
-    });
-  });
-});
 
 describe('unsharp_mask', function () {
-  it('js', function () {
-    const mlib = mathlib_raw({ wasm: false }).use(require('../lib/unsharp_mask'));
 
-    let fixture = createFixture(100, 100);
-    mlib.unsharp_mask(fixture, 100, 100, 80, 2, 2);
+  describe('glur_mono16', function () {
+
+    it('js', function () {
+      const glur_js = require('glur/mono16');
+
+      let sample = new Uint16Array(100 * 100);
+
+      fill(sample, 33333);
+
+      let orig = sample.slice(0, sample.length);
+
+      glur_js(sample, 100, 100, 2);
+
+      assert.deepEqual(sample, orig);
+    });
+
+
+    it('wasm', function () {
+      const glur_js   = require('glur/mono16');
+      const glur_wasm = require('./glur_mono16_wasm');
+      const mlib_wasm = mathlib_raw({ js: false }).use(require('../lib/unsharp_mask'));
+
+      return mlib_wasm.init().then(function () {
+        let sample = new Uint16Array(100 * 100);
+        fill(sample, [ 0, 255 ]);
+
+        let sample_js   = sample.slice(0, sample.length);
+        let sample_wasm = sample.slice(0, sample.length);
+
+        glur_js(sample_js, 100, 100, 2);
+
+        let blur_wasm = glur_wasm(mlib_wasm, sample_wasm, 100, 100, 2);
+
+        assert.deepEqual(sample_js, blur_wasm);
+      });
+    });
   });
 
-  it('wasm', function () {
-    const mlib_js = mathlib_raw({ wasm: false }).use(require('../lib/unsharp_mask'));
-    const mlib_wasm = mathlib_raw({ js: false }).use(require('../lib/unsharp_mask'));
 
-    return mlib_wasm.init().then(function () {
-      let fixture_js = createFixture(100, 100);
-      mlib_js.unsharp_mask(fixture_js, 100, 100, 80, 2, 2);
+  describe('unsharp_mask', function () {
 
-      let fixture_wasm = createFixture(100, 100);
-      mlib_wasm.unsharp_mask(fixture_wasm, 100, 100, 80, 2, 2);
+    function createSample(width, height) {
+      const result = new Uint8Array(width * height * 4);
 
-      assert.deepEqual(fixture_js, fixture_wasm);
+      for (let i = 0; i < result.length; i++) result[i] = 20 + i;
+
+      return result;
+    }
+
+
+    it('js should not throw without wasm', function () {
+      const mlib = mathlib_raw({ wasm: false }).use(require('../lib/unsharp_mask'));
+
+      let sample = createSample(100, 100);
+      mlib.unsharp_mask(sample, 100, 100, 80, 2, 2);
+    });
+
+
+    it('wasm', function () {
+      const mlib_js = mathlib_raw({ wasm: false }).use(require('../lib/unsharp_mask'));
+      const mlib_wasm = mathlib_raw({ js: false }).use(require('../lib/unsharp_mask'));
+
+      return mlib_wasm.init().then(function () {
+        let sample_js   = createSample(100, 100);
+        let sample_wasm = createSample(100, 100);
+
+        mlib_js.unsharp_mask(sample_js, 100, 100, 80, 2, 2);
+        mlib_wasm.unsharp_mask(sample_wasm, 100, 100, 80, 2, 2);
+
+        assert.deepEqual(sample_js, sample_wasm);
+      });
     });
   });
 });
